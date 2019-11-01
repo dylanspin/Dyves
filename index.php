d763640868e0069b83e1d03e9a78e6dadba2c002
dylan <dylanspin100@hotmail.com>
2019-11-01 11:42:00 +0100

    Begin Dyves

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hyves</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" type="image/png" href="pic/logo.png">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  </head>
+  <body>
+
+    <div class="cookie">
+      Dyves kan cookies gebruiken. Klik <button class="cookie_button">Hier</button> voor meer informatie. <button class="cookie_button">Sluiten</button>
+    </div>
+
+    <div class="header">
+      <a class="header_a">Nu & Straks</a>
+      <a class="header_a">vrienden</a>
+      <a class="header_a">agenda</a>
+      <a class="header_a">games</a>
+      <a class="header_a">Nieuws</a>
+      <a class="header_a">meer</a>
+    </div>
+
+
+  </body>
+</html>
